33c7729f9059f1c8e6e670c11241f2e1143418f1
KerstingJ <kersting.josh@gmail.com>
2016-11-10 21:44:53 -0600

    finished log in and sign up functionality

--- src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
@@ -1,9 +1,10 @@
 package org.launchcode.blogz.controllers;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.launchcode.blogz.models.User;
+import org.launchcode.blogz.models.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,35 +13,86 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AuthenticationController extends AbstractController {
 	
+	@Autowired
+	private UserDao userDao;
+	
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signupForm() {
 		return "signup";
 	}
-	
+
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public String signup(HttpServletRequest request, Model model) {
-		
+
 		// TODO - implement signup
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String verify = request.getParameter("verify");
+		System.out.println("Got request headers");
+
+		if (User.isValidUsername(username) && User.isValidPassword(password) && password.equals(verify)) {
+
+			User user = new User(username, password);
+			userDao.save(user);
+			
+			return "newpost";	
+		}
 		
-		return "redirect:blog/newpost";
+		
+		if (!User.isValidPassword(password) || password == null) {
+			//if password not valid return error
+			model.addAttribute("password_error", "Password is invalid");
+			System.out.println("Password failed");
+		}
+		
+		if (!User.isValidUsername(username) || username == null) {
+			//if username not valid return error
+			model.addAttribute("username_error", "Username is Invalid");
+			System.out.println("username failed");
+		}
+		
+		if (!password.equals(verify)) {
+			//if passwords dont match return error
+			model.addAttribute("verify_error", "Passwords do not match");
+			System.out.println("verify failed");
+			
+		}
+		
+		return "signup";
+
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginForm() {
 		return "login";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(HttpServletRequest request, Model model) {
-		
+
 		// TODO - implement login
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 		
-		return "redirect:blog/newpost";
+		User user = userDao.findByUsername(username);
+		if (user == null) {
+			model.addAttribute("error", "Username doesnt exist");
+			return "/login";
+		}
+
+		if (user.isMatchingPassword(password)) {
+			this.setUserInSession(request.getSession(), user);
+			return "redirect:blog/newpost";
+		}
+		
+		model.addAttribute("error", "password is incorrect");
+		return "login";
+		
 	}
-	
+
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public String logout(HttpServletRequest request){
-        request.getSession().invalidate();
+	public String logout(HttpServletRequest request) {
+		request.getSession().invalidate();
 		return "redirect:/";
 	}
 }
--- src/main/java/org/launchcode/blogz/controllers/BlogController.java
+++ src/main/java/org/launchcode/blogz/controllers/BlogController.java
@@ -4,18 +4,30 @@ import java.util.List;
 
 import org.launchcode.blogz.models.Post;
 import org.launchcode.blogz.models.User;
+import org.launchcode.blogz.models.dao.PostDao;
+import org.launchcode.blogz.models.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController extends AbstractController {
-
+	
+	@Autowired
+	UserDao userDao;
+	
+	@Autowired
+	PostDao postDao;
+	
 	@RequestMapping(value = "/")
 	public String index(Model model){
 		
 		// TODO - fetch users and pass to template
 		
+		List<User> users = userDao.findAll();
+		
+		model.addAttribute(users);
 		return "index";
 	}
 	
@@ -23,7 +35,9 @@ public class BlogController extends AbstractController {
 	public String blogIndex(Model model) {
 		
 		// TODO - fetch posts and pass to template
+		Iterable<Post> posts = postDao.findAll();
 		
+		model.addAttribute(posts);
 		return "blog";
 	}
 	
